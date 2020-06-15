d80adfca6a29f88a4d4093a3280de4e8ab57ae7c
Arman <test@mail.ru>
2020-06-15 18:10:39 +0400

    ffddfdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
 
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement(
-  //     'h1',
-  //     null,
-  //     'Hello world!'
-  //   )
-  // )
+  state = {
+    cars: [
+      { name: 'Ford', year: 2018 },
+      { name: 'Audi', year: 2016 },
+      { name: 'Mazda', year: 2010 }
+    ],
+    pageTitle: 'React components',
+    textDescr: 'Arman forefer',
+    showCars: false
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({ cars })
+  }
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({ cars })
+  }
+
+  changeTextHandler = (newText) => {
+    this.setState({
+      textDescr: newText
+    })
+  }
+
+
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    let cars = null
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+            onChangeText={() => this.changeTextHandler(car.year)}
+          />
+        )
+      })
+    }
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+        <button
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
+
+
+        <p>{this.state.textDescr}</p>
+        <button onClick={this.changeTextHandler.bind(this, 'Kruto!!! Rabotaet')}
+        >Change text</button>
+
+        {cars}
+
+      </div>
+    );
+  }
 }
 
 export default App;
